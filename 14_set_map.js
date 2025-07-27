489d499dae71b22c2ee7dc855e089c0d0ed44682
vlad <vlad3eko@gmail.com>
2025-07-27 23:21:50 +0300

    Algorithms: 12_linked_list 13_binary_tree 14_set_map

---
+++ 14_set_map.js
@@ -0,0 +1,18 @@
+const map = new Map()
+const objKey = {id: 5}
+
+map.set(objKey, 'ulbi tv')
+
+console.log(map.get(objKey))
+console.log(map)
+
+const set = new Set()
+
+set.add(5)
+set.add(2)
+set.add(6)
+set.add(5)
+set.add(1)
+set.add(2)
+set.add(3)
+console.log(set)
